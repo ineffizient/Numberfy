990ee24a4d73f7edfe3a07a4e3a4a5f2f768a0bb
Avishek Sen Gupta <avi@deep-thought.fritz.box>
2011-11-27 17:56:19 +0100

    removed some hardcoded stuff and given the user more liberties

--- make-numbers.py
+++ make-numbers.py
@@ -7,7 +7,7 @@ from gimpfu import *
 import glob
 import random
  
-def make_numbers(image, drawable, width, height, special_number, bg_color, fg_color, hl_color, size) : 
+def make_numbers(image, drawable, width, height, flag, special_number, bg_color, fg_color, hl_color, size) : 
 	# create image in the first place
 	
 	new_image = gimp.Image(width, height, 0)
@@ -23,15 +23,17 @@ def make_numbers(image, drawable, width, height, special_number, bg_color, fg_co
 	gimp.set_foreground(fg_color) 
 	n_lines = height / size
  	x_position = 0
-	magic_number = 0 
+	cont_number = 0 
 	current_line = ""
-#	pdb.gimp_progress_init("Working...", '-1') 
 	for i in range (0,n_lines) : 
 #		print "I am in line " + str(n_lines)
 		while (x_position < width) : 
 #			print "x_position is " + str(x_position)
 			num = random.randint(0,9)
-			hl_flag = (num == magic_number) 
+			if (flag): 
+				hl_flag = (num == special_number) 
+			else: 
+				hl_flag = (num == cont_number) 
 			extent = pdb.gimp_text_get_extents_fontname(str(num), size, 0, "Sans")
 			if (extent[1] + x_position > width) : 
 				break
@@ -39,13 +41,11 @@ def make_numbers(image, drawable, width, height, special_number, bg_color, fg_co
 				if (hl_flag): 
 				        gimp.set_foreground(hl_color)
 
-#					hl_color = pdb.gimp_image_pick_color(image, drawable, x_position, i*size, 1, 1, size)
-					if (magic_number == 9): 
-						magic_number = 0
+					if (cont_number == 9): 
+						cont_number = 0
 					else: 
-						magic_number = magic_number + 1
+						cont_number = cont_number + 1
 				else: 
-#                                        fg_color = pdb.gimp_image_pick_color(image, drawable, x_position, i*size, 1, 1, size)
 					gimp.set_foreground(fg_color) 
 
 		                pdb.gimp_text_fontname(new_image,background_layer , x_position, i*size, str(num), 0, 1, size, 0, "Sans") 
@@ -70,6 +70,7 @@ register(
 	[
 		(PF_INT, 'width', 'The width of the area to be filled', 1280), 
 		(PF_INT, 'height', 'The height of the area to be filled', 1024),
+		(PF_BOOL, 'highlight_mode', 'Highlight a special number (YES) or highlight continous numbers (NO)', 1), 
 		(PF_INT, 'special_number', 'The digit to be highlighted', 0),
 		(PF_COLOR, 'background', 'The color of the background', (0,0,0)),
 		(PF_COLOR, 'foreground', 'The color of the numbers.', (0,0,0) ), 
